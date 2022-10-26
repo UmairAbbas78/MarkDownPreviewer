2f2d06bd426202ada7fe9efba6b61e733efb0066
Umair Abbas <windrgames@gmail.com>
2022-10-26 13:41:13 +0500

    Working App Ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+
+  
+
+  const[textValue,setTextValue]=React.useState("");
+
+  function handleChange(event){
+    setTextValue(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Mark Down Preview</h1>
       </header>
+      <main className='main'>        
+       <div>
+          <textarea className='markDown'
+                onChange={handleChange}
+                name='text'
+                value={textValue}
+                placeholder="write here"
+            />
+        </div>
+          <div className='rendText'>
+          <ReactMarkdown>{textValue}</ReactMarkdown>
+          </div>
+      </main>
     </div>
   );
 }
