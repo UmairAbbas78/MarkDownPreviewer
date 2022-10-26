f1e50634159ff6410d13ac93f5b4c8ba0e3cfbb3
Umair Abbas <windrgames@gmail.com>
2022-10-26 14:55:45 +0500

    All Tasks Complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import './App.css';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function App() {
 
-  
-
-  const[textValue,setTextValue]=React.useState("");
+  const[textValue,setTextValue]=React.useState(`# H1\n`+`## H2`+"\n[link](https://www.freecodecamp.org)"+`\n> Block Quotes!
+  `+`![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`+`\n1. And there are numbered lists too.\n1. Use just 1s if you want! \n1.`+
+  `\n**bold**`+"\n```js \nfunction var => { None }\n```"+"\n``` this is code ```")
 
   function handleChange(event){
     setTextValue(event.target.value);
   }
+  
 
   return (
+    
     <div className="App">
       <header className="App-header">
         <h1>Mark Down Preview</h1>
       </header>
       <main className='main'>        
        <div>
-          <textarea className='markDown'
+          <textarea
+                id="editor" 
+                className='markDown'
                 onChange={handleChange}
                 name='text'
                 value={textValue}
                 placeholder="write here"
             />
         </div>
-          <div className='rendText'>
+          <div className='rendText' id="preview">
           <ReactMarkdown>{textValue}</ReactMarkdown>
           </div>
       </main>
     </div>
   );
 }
-
+const Component = (value) => {
+  return (
+    <SyntaxHighlighter language="javascript" style={dark}>
+      {value}
+    </SyntaxHighlighter>
+  );
+};
 export default App;
